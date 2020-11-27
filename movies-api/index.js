cc70de31de6a7df526d55bd8f0621f104241a2e0
Aaron Tierney Smith <smithaaron2000@gmail.com>
2020-11-27 15:48:33 +0000

    added routing for add, update and delete movies

--- movies-api/index.js
+++ movies-api/index.js
@@ -1,10 +1,15 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './api/movies';
+import bodyParser from 'body-parser';
 
 dotenv.config();
 
 const app = express();
+
+//configure body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
 
 const port = process.env.PORT;
 
