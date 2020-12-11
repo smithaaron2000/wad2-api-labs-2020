2a614bec6657045e06119f34d7b2a83ee304079c
Aaron Tierney Smith <smithaaron2000@gmail.com>
2020-12-11 20:26:51 +0000

    Week 9 User Favourites

--- movies-api/api/users/index.js
+++ movies-api/api/users/index.js
@@ -4,9 +4,10 @@ import User from './userModel';
 const router = express.Router(); // eslint-disable-line
 
 // Get all users
-router.get('/', (req, res) => {
-    User.find().then(users =>  res.status(200).json({success:true,token:"FakeTokenForNow"})).catch(next);
+router.get('/', (req, res, next) => {
+    User.find().then(users =>  res.status(200).json(users)).catch(next);
 });
+
 
 // register
 router.post('/', (req, res ,next) => {
@@ -14,13 +15,38 @@ router.post('/', (req, res ,next) => {
 });
 
 // Update a user
-router.put('/:id',  (req, res) => {
+router.put('/:id',  (req, res, next) => {
     if (req.body._id) delete req.body._id;
      User.update({
       _id: req.params.id,
     }, req.body, {
       upsert: false,
     })
-    .then(user => res.status(200).json({success:true,token:"FakeTokenForNow"})).catch(next);
+    .then(user => res.json(200, user)).catch(next);
 });
+
+router.get('/:userName/favourites', (req, res, next) => {
+  const user = req.params.userName;
+  User.find( {username: user}).then(
+      user => res.status(201).send(user.favourites)
+  ).catch(next);
+});
+
+router.post('/:userName/favourites', (req, res, next) => {
+  const newFavourite = req.body;
+  const query = {username: req.params.userName};
+  if (newFavourite && newFavourite.id) {
+    User.find(query).then(
+      user => {
+        (user.favourites)?user.favourites.push(newFavourite):user.favourites =[newFavourite];
+        User.findOneAndUpdate(query, {favourites:user.favourites}, {
+          new: true
+        }).then(user => res.status(201).send(user));
+      }
+    ).catch(next);
+  } else {
+      res.status(401).send("Unable to find user");
+  }
+});
+
 export default router;
